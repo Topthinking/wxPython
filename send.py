b9c99df9959a8ea3f2fc7e3e36a0ecae4a0a64f8
top <18301866067@163.com>
2017-06-28 16:46:29 +0800

    new

--- send.py
+++ send.py
@@ -2,4 +2,4 @@ from app.server.send_update_notify import VerUpdateSend
 
 if __name__ == '__main__':
     wx_send = VerUpdateSend(True)
-    wx_send.sendMsg("欢迎使用top机器人，更多信息访问https://github.com/Topthinking/wxPython/")
+    wx_send.sendMsg("欢迎使用top机器人,更多信息访问https://github.com/Topthinking/wxPython/")
