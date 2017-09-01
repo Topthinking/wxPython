bcffc375c0d754dbcd2ebdcecfc4dd41b34e60cb
topthinking <topthinking@ximalaya.com>
2017-09-01 10:01:04 +0800

    请求斗鱼接口时 添加user-agent

--- app/douyu/html_downloader.py
+++ app/douyu/html_downloader.py
@@ -6,15 +6,21 @@ class HtmlDownloader(object):
     
     
     def download(self,url):
+        print(url)
+
         if url is None:
             return None
+
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+
+        req = urllib.request.Request(url=url, headers=headers) 
         
-        response = urllib.request.urlopen(url)
+        response = urllib.request.urlopen(req)
         
         if response.getcode() != 200:
             return None
         
-        return response.read()
+        return response.read().decode()
     
     
 
